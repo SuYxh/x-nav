3bce1d8e6594884982373a7207eb8c7d114cf9fa
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-05-03 13:07:03 +0800

    feat: update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,8 @@ const isDev = process.env.NODE_ENV === 'development'
 export default defineNuxtConfig({
   // devtools: { enabled: true }, //开发工具
   app: {
-    baseURL: isDev ? '/' : '/abinWeb/',
+    // baseURL: isDev ? '/' : '/abinWeb/',
+    baseURL: '/',
     buildAssetsDir: 'static', //修改站点资产的文件夹名称，默认是_nuxt
     head: {
       title: '前端助手',
